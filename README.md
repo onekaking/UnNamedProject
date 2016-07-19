9648779e9db43062fd4d2d21bc93c7c34d92f3c4
thien.to <thien.to@niteco.se>
2016-07-19 10:57:48 +0700

    *FirstCommit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UnNamedProject
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/MajorController.js
@@ -0,0 +1,11 @@
+/**
+ * MajorController
+ *
+ * @description :: Server-side logic for managing Majors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
